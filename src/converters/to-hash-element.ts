1b67dc5716b88230fd2d9021604d91feabf5eabe
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-05 18:05:10 +0530

    adds function to hash element

--- src/converters/to-hash-element.ts
+++ src/converters/to-hash-element.ts
@@ -1,3 +1,19 @@
+import {getHtmlBlock} from "../service/get-html-block";
+
 export function toHashElement(text: string): string {
-    return '';
+    // @ts-ignore
+    let blockText = m1;
+
+    // Undo double lines
+    blockText = blockText.replace(/\n\n/g, '\n');
+    blockText = blockText.replace(/^\n/, '');
+
+    // strip trailing blank lines
+    blockText = blockText.replace(/\n+$/g, '');
+
+    // Replace the element text with a marker ("¨KxK" where x is its key)
+    // @ts-ignore
+    blockText = '\n\n¨K' + (getHtmlBlock().push(blockText) - 1) + 'K\n\n';
+
+    return blockText;
 }
