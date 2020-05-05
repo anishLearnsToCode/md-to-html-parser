a05becea8f5b3b1c448298b0d61fd0f5b4c422f5
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-05 19:14:33 +0530

    adds hash html span function

--- src/service/hash-html-span.ts
+++ src/service/hash-html-span.ts
@@ -1,3 +1,23 @@
 export function hashHtmlSpan(text: string): string {
-    return '';
+    // Hash Self Closing tags
+    text = text.replace(/<[^>]+?\/>/gi, function (wm) {
+        return hashHtmlSpan(wm);
+    });
+
+    // Hash tags without properties
+    text = text.replace(/<([^>]+?)>[\s\S]*?<\/\1>/g, function (wm) {
+        return hashHtmlSpan(wm);
+    });
+
+    // Hash tags with properties
+    text = text.replace(/<([^>]+?)\s[^>]+?>[\s\S]*?<\/\1>/g, function (wm) {
+        return hashHtmlSpan(wm);
+    });
+
+    // Hash self closing tags without />
+    text = text.replace(/<[^>]+?>/gi, function (wm) {
+        return hashHtmlSpan(wm);
+    });
+
+    return text;
 }
