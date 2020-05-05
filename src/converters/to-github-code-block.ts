6a56d9553f21112fd3e8b098d8495c1d8d6695ac
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-05 17:55:33 +0530

    adds encode code function

--- src/converters/to-github-code-block.ts
+++ src/converters/to-github-code-block.ts
@@ -1,3 +1,31 @@
+import {toHashBlock} from "./to-hash-block";
+import {encodeCode} from "../service/encode-code";
+import {removeTab} from "./remove-tab";
+
 export function toGitHubCodeBlock(text: string): string {
-    return '';
+    text += '¨0';
+
+    text = text.replace(/(?:^|\n)(?: {0,3})(```+|~~~+)(?: *)([^\s`~]*)\n([\s\S]*?)\n(?: {0,3})\1/g,
+        (wholeMatch, delim, language, codeblock) => {
+
+        // First parse the github code block
+        codeblock = encodeCode(codeblock);
+        codeblock = removeTab(codeblock);
+        codeblock = codeblock.replace(/^\n+/g, ''); // trim leading newlines
+        codeblock = codeblock.replace(/\n+$/g, ''); // trim trailing whitespace
+
+        codeblock = '<pre><code'
+            + (language ? ' class="' + language + ' language-' + language + '"' : '')
+            + '>'
+            + codeblock
+            + '</code></pre>';
+
+        codeblock = toHashBlock(codeblock);
+
+        return codeblock;
+    });
+
+    text = text.replace(/¨0/, '');
+
+    return text;
 }
