f690ed318ac75d3e1be9f491751e26bd4dbff2d8
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-05 19:01:16 +0530

    adds to horizontal rule function

--- src/converters/to-horizontal-rule.ts
+++ src/converters/to-horizontal-rule.ts
@@ -1,3 +1,10 @@
+import {toHashBlock} from "./to-hash-block";
+
 export function toHorizontalRule(text: string): string {
-    return '';
+    const key = toHashBlock('<hr />');
+    text = text.replace(/^ {0,2}( ?-){3,}[ \t]*$/gm, key);
+    text = text.replace(/^ {0,2}( ?\*){3,}[ \t]*$/gm, key);
+    text = text.replace(/^ {0,2}( ?_){3,}[ \t]*$/gm, key);
+    
+    return text;
 }
